c73c70778e452ce166c783eb314152d2604fe49a
timbuethe <timbuethe@44d63d7b-0b3e-0410-96a1-c7ce24e4b5df>
2008-10-02 15:37:10 +0000

    - added "SORT_DIGG_COUNT_ASC" and "SORT_DIGG_COUNT_DESC" to setSort() - added minDate and maxDate - added media

--- jigg/src/de/thesuntoucher/jigg/args/StoriesArguments.java
+++ jigg/src/de/thesuntoucher/jigg/args/StoriesArguments.java
@@ -3,9 +3,12 @@
  */
 package de.thesuntoucher.jigg.args;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import de.thesuntoucher.jigg.Jigg;
+import de.thesuntoucher.jigg.data.Media;
 
 /**
  * implements <a href="http://apidoc.digg.com/ListStories#Arguments">this list of arguments</a>
@@ -24,10 +27,16 @@ public class StoriesArguments extends Arguments<StoriesArguments> {
 	
 	/** oldest-submitted first */
 	public static final String SORT_SUBMIT_DATE_ASC = "submit_date-asc";
+
+	/** lowest digg count first */
+	public static final String SORT_DIGG_COUNT_ASC = "digg_count-asc";
 	
-	private Date minSubmit, maxSubmit, minPromote, maxPromote;
+	/** highest digg count first */
+	public static final String SORT_DIGG_COUNT_DESC = "digg_count-desc";
+	
+	private Date minSubmit, maxSubmit, minPromote, maxPromote, minDate, maxDate;
 	private String domain, link, sort;
-	
+	private List<Media> media;
 	
 	/**
 	 * @param minSubmit the minSubmit to set
@@ -84,6 +93,23 @@ public class StoriesArguments extends Arguments<StoriesArguments> {
 	}
 
 	/**
+	 * @param media
+	 * @return this
+	 */
+	public StoriesArguments setMedia(Media... media) {
+		this.media = Arrays.asList(media);
+		return this;
+	}
+	
+	private String getMediaString(){
+		StringBuilder sb = new StringBuilder();
+		for (Media media : this.media) {
+			sb.append(media.getShortName());
+		}
+		return sb.toString();
+	}
+
+	/**
 	 * @param sort the sort to set
 	 * @return this
 	 */
@@ -92,7 +118,9 @@ public class StoriesArguments extends Arguments<StoriesArguments> {
 		if(	!sort.equals(SORT_PROMOTE_DATE_ASC) &&
 			!sort.equals(SORT_PROMOTE_DATE_DESC) &&
 			!sort.equals(SORT_SUBMIT_DATE_ASC) &&
-			!sort.equals(SORT_SUBMIT_DATE_DESC)){
+			!sort.equals(SORT_SUBMIT_DATE_DESC) &&
+			!sort.equals(SORT_DIGG_COUNT_ASC) &&
+			!sort.equals(SORT_DIGG_COUNT_DESC)){
 			
 			throw new IllegalArgumentException("the argument sort invalid. sort: " + sort);
 		}
@@ -111,8 +139,11 @@ public class StoriesArguments extends Arguments<StoriesArguments> {
 			(maxSubmit != null ? "&max_submit_date=" + maxSubmit.getTime() / 1000 : "") + 
 			(minPromote != null ? "&min_promote_date=" + minPromote.getTime() / 1000 : "") + 
 			(maxPromote != null ? "&max_promote_date=" + maxPromote.getTime() / 1000 : "") + 
+			(minDate != null ? "&min_date=" + minDate.getTime() / 1000 : "") + 
+			(maxDate != null ? "&max_date=" + maxDate.getTime() / 1000 : "") + 
 			(domain != null ? "&domain=" + domain : "") + 
 			(link != null ? "&link=" + link : "") + 
+			(media != null && media.size() > 0 ? "&media=" + getMediaString() : "") + 
 			(sort != null ? "&sort=" + sort : "");
 	}
 }
